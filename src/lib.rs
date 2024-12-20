c53165d6613611f7a1786d03efb04446d2fc03d9
HAYASAKA Ryosuke <hayasaka.ryosuke@gmail.com>
2024-12-21 04:47:58 +0900

    wasmの足りないファイルを追加

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+mod tokenizer;
+mod parser;
+mod environment;
+mod eval;
+mod wasm;
+
+pub use wasm::evaluate;
---
+++ src/wasm.rs
@@ -0,0 +1,15 @@
+use wasm_bindgen::prelude::*;
+use crate::tokenizer::tokenize;
+use crate::parser::Parser;
+use crate::environment::Env;
+use crate::eval::{eval, evals};
+
+#[wasm_bindgen]
+pub fn evaluate(input: &str) -> String {
+    let tokens = tokenize(&input.to_string());
+    let mut parser = Parser::new(tokens);
+    let ast_nodes = parser.parse_lines();
+    let mut env = Env::new();
+    let result = evals(ast_nodes, &mut env);
+    format!("{:?}", result)
+} 
