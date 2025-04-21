8292d72ebec08e68c3c8b106132b15af39c808dc
HAYASAKA Ryosuke <hayasaka.ryosuke@gmail.com>
2025-04-22 05:07:08 +0900

    fix pattern match type

--- src/parsers/match_ast.rs
+++ src/parsers/match_ast.rs
@@ -2,43 +2,18 @@ use crate::ast::ASTNode;
 use crate::parsers::Parser;
 use crate::token::TokenKind;
 use crate::parsers::parse_error::ParseError;
-use crate::environment::ValueType;
+use crate::environment::{EnvVariableType, ValueType};
 
 impl Parser {
-
-    fn get_type(&self, ast: &ASTNode) -> Option<ValueType> {
-        match &ast {
-            ASTNode::Literal { value, .. } => {
-                Some(value.value_type())
-            }
-            ASTNode::Variable { value_type, .. } => {
-                value_type.clone()
-            }
-            ASTNode::Block { nodes, .. } => {
-                println!("nodes: {:?}", nodes);
-                if let Some(node) = nodes.last() {
-                    match node {
-                        ASTNode::Literal { value, .. } => Some(value.value_type()),
-                        ASTNode::Variable { value_type, .. } => value_type.clone(),
-                        _ => None,
-                    }
-                } else {
-                    None
-                }
-            }
-            _ => {
-                None
-            }
-        }
-    }
 
     pub fn parse_match(&mut self) -> Result<ASTNode, ParseError> {
         self.consume_token();
         let expression = self.parse_expression(0)?;
         self.extract_token(TokenKind::LBrace);
         let mut cases = vec![];
-        let mut case_pattern_type: Option<ValueType> = None;
+        let case_pattern_type: Result<ValueType, String> = self.infer_type(&expression);
         let mut case_body_type: Option<ValueType> = None;
+        let mut under_score_pattern_count = 0;
         while self.get_current_token().is_some() && self.get_current_token().unwrap().kind != TokenKind::RBrace {
             if self.get_current_token().unwrap().kind == TokenKind::Eof {
                 self.pos = 0;
@@ -46,23 +21,42 @@ impl Parser {
                 continue;
             }
             let pattern = self.parse_expression(0)?;
+            match pattern {
+                ASTNode::OptionSome { ref value, .. } => {
+                    match *value.clone() {
+                        ASTNode::Variable { name, .. } => {
+                            let expression_type = self.infer_type(&expression);
+                            self.register_variables(self.get_current_scope().clone(), &name, &expression_type.unwrap(), &EnvVariableType::Immutable);
+                        }
+                        _ => {}
+                    };
+                }
+                _ => {}
+            };
             self.extract_token(TokenKind::RRocket);
             let body = self.parse_block()?;
             cases.push((pattern.clone(), body.clone()));
-            if case_pattern_type.is_none() {
-                case_pattern_type = self.get_type(&pattern);
-                if case_pattern_type.is_none() {
-                    return Err(ParseError::new("Unsupported pattern", &self.get_current_token().unwrap()))
+            let is_underscore = match pattern {
+                    ASTNode::Variable { ref name, .. } => {
+                        name == "_"
+                    }
+                    _ => false
+            };
+            if is_underscore {
+                under_score_pattern_count += 1;
+                if under_score_pattern_count >= 2 {
+                    return Err(ParseError::new("too many wild card pattern _", &self.get_current_token().unwrap()))
                 }
-            } else if case_pattern_type != self.get_type(&pattern) {
-                return Err(ParseError::new("Pattern type mismatch", &self.get_current_token().unwrap()));
+            }
+            if !is_underscore && self.infer_type(&pattern).is_err() {
+                return Err(ParseError::new("Unsupported pattern", &self.get_current_token().unwrap()))
             }
             if case_body_type.is_none() {
-                case_body_type = self.get_type(&body);
-                if case_body_type.is_none() {
-                    return Err(ParseError::new("Unsupported pattern", &self.get_current_token().unwrap()));
+                case_body_type = self.infer_type(&body).ok();
+                if case_pattern_type.is_err() {
+                    return Err(ParseError::new("Pattern type mismatch", &self.get_current_token().unwrap()));
                 }
-            } else if case_body_type != self.get_type(&body) {
+            } else if case_body_type != self.infer_type(&body).ok() {
                 return Err(ParseError::new("Pattern type mismatch", &self.get_current_token().unwrap()));
             }
         }
@@ -92,7 +86,7 @@ mod tests {
         let mut parser = Parser::new(vec![
             Token{kind: TokenKind::Match, line: 1, column: 1},
             Token{kind: TokenKind::LParen, line: 1, column: 7},
-            Token{kind: TokenKind::Identifier("a".to_string()), line: 1, column: 8},
+            Token{kind: TokenKind::Number(Fraction::from(0)), line: 1, column: 8},
             Token{kind: TokenKind::RParen, line: 1, column: 9},
             Token{kind: TokenKind::LBrace, line: 1, column: 11},
             Token{kind: TokenKind::Number(Fraction::from(1)), line: 1, column: 13},
@@ -107,7 +101,7 @@ mod tests {
         let ast = result.unwrap();
         match ast {
             ASTNode::Match { expression, cases, .. } => {
-                assert_eq!(*expression, ASTNode::Variable{name: "a".to_string(), value_type: None, line: 0, column: 3});
+                assert_eq!(*expression, ASTNode::Literal{value: Value::Number(Fraction::from(0)), line: 1, column: 9});
                 assert_eq!(cases.len(), 1);
                 assert_eq!(cases[0].0, ASTNode::Literal{value: Value::Number(Fraction::from(1)), line: 1, column: 15});
                 assert_eq!(cases[0].1, ASTNode::Block{nodes: vec![ASTNode::Literal{value: Value::Number(Fraction::from(2)), line: 1, column: 20}], line: 1, column: 20});
