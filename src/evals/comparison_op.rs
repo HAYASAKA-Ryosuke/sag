042b3800d8c60f3d5a54ae586e3de47c47765ca3
HAYASAKA Ryosuke <hayasaka.ryosuke@gmail.com>
2025-06-01 23:36:14 +0900

    add string comparison_op

--- src/evals/comparison_op.rs
+++ src/evals/comparison_op.rs
@@ -10,10 +10,15 @@ pub fn comparison_op_node(op: TokenKind, left: Box<ASTNode>, right: Box<ASTNode>
     let right_value = eval(*right, env)?;
     match (left_value, right_value, op) {
         (Value::Number(l), Value::Number(r), TokenKind::Eq) => Ok(Value::Bool(l == r)),
+        (Value::Number(l), Value::Number(r), TokenKind::Neq) => Ok(Value::Bool(l != r)),
         (Value::Number(l), Value::Number(r), TokenKind::Gte) => Ok(Value::Bool(l >= r)),
         (Value::Number(l), Value::Number(r), TokenKind::Gt) => Ok(Value::Bool(l > r)),
         (Value::Number(l), Value::Number(r), TokenKind::Lte) => Ok(Value::Bool(l <= r)),
         (Value::Number(l), Value::Number(r), TokenKind::Lt) => Ok(Value::Bool(l < r)),
+        (Value::String(l), Value::String(r), TokenKind::Eq) => Ok(Value::Bool(l == r)),
+        (Value::String(l), Value::String(r), TokenKind::Neq) => Ok(Value::Bool(l != r)),
+        (Value::Bool(l), Value::Bool(r), TokenKind::Eq) => Ok(Value::Bool(l == r)),
+        (Value::Bool(l), Value::Bool(r), TokenKind::Neq) => Ok(Value::Bool(l != r)),
         _ => Err(RuntimeError::new("Unsupported operation", line, column)),
     }
 }
--- src/token.rs
+++ src/token.rs
@@ -43,6 +43,7 @@ pub enum TokenKind {
     If,
     Else,
     Eq,
+    Neq,
     Lte,
     Lt,
     Gte,
